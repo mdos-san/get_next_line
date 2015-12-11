7b5f6249479fcfa53863d1acd9fb0be92fab02ec
Mickael DOS SANTOS <mdos-san@e2r9p5.42.fr>
2015-12-11 20:00:50 +0100

    first step of normalizing.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 18:09:33 by mdos-san          #+#    #+#             */
-/*   Updated: 2015/12/11 19:15:16 by mdos-san         ###   ########.fr       */
+/*   Updated: 2015/12/11 19:56:55 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,36 @@ static int	lengt_line(char *str)
 	return (i + 1);
 }
 
+static int	realloc_str(char ***line, char **st_str, char **tmp, int *len)
+{
+	**line = ft_strdup(*st_str);
+	if (*st_str)
+		free(*st_str);
+	*st_str = (char*)malloc(*len + 1);
+	if (!*st_str)
+		return (0);
+	ft_bzero(*st_str, *len);
+	ft_strcat(*st_str, **line);
+	if (**line)
+		free(**line);
+	ft_strcat(*st_str, *tmp);
+	ft_bzero(*tmp, BUFF_SIZE);
+	return (1);
+}
+
+static void	truncate_str(char ***line, char **st_str, char **tmp)
+{
+	free(*tmp);
+	*tmp = ft_strchr(*st_str, '\n');
+	**tmp = 0;
+	**line = ft_strdup(*st_str);
+	**tmp = '\n';
+	*tmp = ft_strdup(*st_str + lengt_line(*st_str));
+	if (*st_str)
+		free(*st_str);
+	*st_str = *tmp;
+}
+
 int		get_next_line(int const fd, char **line)
 {
 	static char	*st_str = 0;
@@ -34,11 +64,7 @@ int		get_next_line(int const fd, char **line)
 	int			ret;
 
 	len = ft_strlen(st_str);
-	tmp = (char*)malloc(BUFF_SIZE + 1);
-	if (!tmp)
-		return (-1);
-	tmp[BUFF_SIZE] = 0;
-	ft_bzero(tmp, BUFF_SIZE);
+	tmp = ft_strnew(BUFF_SIZE);
 	while (!ft_strchr(st_str, '\n'))
 	{
 		ret = read(fd, tmp, BUFF_SIZE);
@@ -49,34 +75,15 @@ int		get_next_line(int const fd, char **line)
 			*line = ft_strdup(st_str);
 			if (ft_strlen(*line) > 0)
 			{
-				free(st_str);
-				st_str = NULL;
+				ft_strdel(&st_str);
 				return (1);
 			}
 			return (0);
 		}
 		len += ret;
-		*line = ft_strdup(st_str);
-		if (st_str)
-			free(st_str);
-		st_str = (char*)malloc(len +1);
-		if (!st_str)
+		if (!realloc_str(&line, &st_str, &tmp, &len))
 			return (-1);
-		ft_bzero(st_str, len);
-		ft_strcat(st_str, *line);
-		if (*line)
-			free(*line);
-		ft_strcat(st_str, tmp);
-		ft_bzero(tmp, BUFF_SIZE);
 	}
-	free(tmp);
-	tmp = ft_strchr(st_str, '\n');
-	*tmp = 0;
-	*line = ft_strdup(st_str);
-	*tmp = '\n';
-	tmp = ft_strdup(st_str + lengt_line(st_str));
-	if (st_str)
-		free(st_str);
-	st_str = tmp;
+	truncate_str(&line, &st_str, &tmp);
 	return (1);
 }
