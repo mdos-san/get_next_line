91b6ac10025a153c5026d2b5744f2dce4b244dd9
Mickael DOS SANTOS <mdos-san@e2r9p5.42.fr>
2015-12-11 20:19:05 +0100

    removing compile.sh

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-make -C libft/ fclean
-make -C libft/
-gcc -Wall -Wextra -Werror -I libft/includes/ -c get_next_line.c
-gcc -Wall -Wextra -Werror -I libft/includes/ -c main.c
-gcc -o test_gnl get_next_line.o main.o -L libft/ -lft
-make -C libft/ fclean
-rm -rf main.o get_next_line.o
