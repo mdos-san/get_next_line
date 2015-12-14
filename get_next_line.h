2143b78c0cba916ddc2f59efa03446fdf2c960e6
Mickael DOS SANTOS <mdos-san@e2r8p10.42.fr>
2015-12-14 15:17:54 +0100

    changing buffer

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 18:08:51 by mdos-san          #+#    #+#             */
-/*   Updated: 2015/12/14 14:51:34 by mdos-san         ###   ########.fr       */
+/*   Updated: 2015/12/14 15:15:18 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "libft.h"
-# define BUFF_SIZE 7
+# define BUFF_SIZE 1024
 
 int	get_next_line(int const fd, char **line);
 
