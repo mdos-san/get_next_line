8b4acc7dc1049946ce7c4a923dde73111d04b3a9
Mickael DOS SANTOS <mdos-san@e2r5p15.42.fr>
2015-12-27 17:28:44 +0100

    Now have an unallocated buffer

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/09 18:09:33 by mdos-san          #+#    #+#             */
-/*   Updated: 2015/12/27 17:20:24 by mdos-san         ###   ########.fr       */
+/*   Updated: 2015/12/27 17:27:05 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static int	lengt_line(char *str)
 	return (i + 1);
 }
 
-static int	realloc_str(char ***line, char **st_str, char **tmp, int *len)
+static int	realloc_str(char ***line, char **st_str, char *buf, int *len)
 {
 	**line = ft_strdup(*st_str);
 	ft_strdel(st_str);
@@ -34,14 +34,12 @@ static int	realloc_str(char ***line, char **st_str, char **tmp, int *len)
 	ft_bzero(*st_str, *len);
 	ft_strcat(*st_str, **line);
 	ft_strdel(*line);
-	ft_strcat(*st_str, *tmp);
-	ft_bzero(*tmp, BUFF_SIZE);
+	ft_strcat(*st_str, buf);
 	return (1);
 }
 
 static void	truncate_str(char ***line, char **st_str, char **tmp)
 {
-	free(*tmp);
 	*tmp = ft_strchr(*st_str, '\n');
 	**tmp = 0;
 	**line = ft_strdup(*st_str);
@@ -66,21 +64,23 @@ int			get_next_line(int const fd, char **line)
 {
 	static char	*st_str = 0;
 	char		*tmp;
+	char		buf[BUFF_SIZE + 1];
 	int			len;
 	int			ret;
 
+	buf[BUFF_SIZE] = 0;
 	len = ft_strlen(st_str);
-	tmp = ft_strnew(BUFF_SIZE);
 	if (fd < 0 || line == 0)
 		return (-1);
 	while (!ft_strchr(st_str, '\n'))
 	{
-		if ((ret = read(fd, tmp, BUFF_SIZE)) == -1)
+		ft_bzero(buf, BUFF_SIZE);
+		if ((ret = read(fd, buf, BUFF_SIZE)) == -1)
 			return (-1);
 		if (ret == 0)
 			return (end_file(&line, &st_str));
 		len = len + ret;
-		if (!realloc_str(&line, &st_str, &tmp, &len))
+		if (!realloc_str(&line, &st_str, buf, &len))
 			return (-1);
 	}
 	truncate_str(&line, &st_str, &tmp);
